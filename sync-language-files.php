67fe7743c701292cf1da757fa5c8d0dcb65adae6
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2019-07-04 16:41:09 +0530

    PLANET-3714 Use a new tag (lang.master3)

--- sync-language-files.php
+++ sync-language-files.php
@@ -99,7 +99,7 @@ class LocoWebhook {
 			$auth_token = base64_encode( $cci_private_token . ':' );
 			$json       = wp_json_encode(
 				[
-					'tag' => 'lang.master2',
+					'tag' => 'lang.master3',
 				]
 			);
 
